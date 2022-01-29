6abf0fd3e51f455b78798feea2f0fc0ee67d84ae
BlueVein <james@blueve.in>
2022-01-29 14:08:15 -0600

    Comprsesed team images moved to new dir

--- web/src/components/Team.js
+++ web/src/components/Team.js
@@ -1,12 +1,12 @@
 import React from "react"; //imports react
-import simon from "../imgs/simon.png";
-import lex from "../imgs/lex.jpg";
-import tim from "../imgs/tim.jpg";
-import elliot from "../imgs/elliot.jpg";
-import james from "../imgs/james.png";
-import nikko from "../imgs/nikko.jpg";
-import gwen from "../imgs/gwen.jpg";
-import luke from "../imgs/luke.jpeg";
+import simon from "../team_imgs/simon.jpg";
+import lex from "../team_imgs/lex.jpg";
+import tim from "../team_imgs/tim.jpg";
+import elliot from "../team_imgs/elliot.jpg";
+import james from "../team_imgs/james.jpg";
+import nikko from "../team_imgs/nikko.jpg";
+import gwen from "../team_imgs/gwen.jpg";
+import luke from "../team_imgs/luke.jpg";
 
 import linkedinLogo from "../imgs/linkedinLogo.png";
 
