99fe8577afd1aee1884172c84ceae941fc231587
Simon Mahns <ak.simonm@gmail.com>
2022-02-08 02:22:56 -0600

    team update

--- web/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react"; //imports react
-
-class Home extends React.Component {
-  render() {
-    return <div className="mainDiv"></div>;
-  }
-}
-
-export default Home;
--- web/src/components/Team.js
+++ web/src/components/Team.js
@@ -1,16 +1,8 @@
-import React from "react"; //imports react
-import simon from "../team_imgs/simon.jpg";
-import lex from "../team_imgs/lex.jpg";
-import tim from "../team_imgs/tim.jpg";
-import elliot from "../team_imgs/elliot.jpg";
-import james from "../team_imgs/james.jpg";
-import nikko from "../team_imgs/nikko.jpg";
-import gwen from "../team_imgs/gwen.jpg";
-import luke from "../team_imgs/luke.jpg";
+import React from "react";
 import FadeIn from "react-fade-in";
-
+import teamData from "./team.json";
 import linkedinLogo from "../imgs/linkedinLogo.png";
-
+import twitterLogo from "../imgs/twitter.png";
 class Team extends React.Component {
   render() {
     return (
@@ -19,164 +11,74 @@ class Team extends React.Component {
           <p className="pageTitle">Team</p>
           <br />
           <FadeIn transitionDuration={1100}>
-            <div className="row ">
-              <div className="profileDiv">
-                <div className="profileImgDiv">
-                  <img src={elliot} className="profileImg" alt="elliot" />
+            {teamData.map((teamTeir) => (
+              <div className="team tempCenter">
+                <p className="regularTitle">{teamTeir.teamTeir}</p>
+                <br />
+                <div className="row wrap">
+                  {teamTeir.members.map((teamMember) => (
+                    <div className="profileDiv">
+                      <div className="profileImgDiv">
+                        <img
+                          src={
+                            require("../teamImgs/" + teamMember.imagePath)
+                              .default
+                          }
+                          className="profileImg"
+                          alt={teamMember.name}
+                        />
+                      </div>
+                      <div className="profileTextDiv">
+                        <p className="profileSubtitle">{teamMember.name}</p>
+                        <i className="profileSubSubtitle">
+                          {teamMember.subtitle}
+                          <br />
+                          {teamMember.affiliates}
+                        </i>
+                        <br />
+                        <div className="rowSocial centerRow teamSocialsDiv">
+                          {teamMember.twitter === "" ? null : (
+                            <a
+                              href={teamMember.twitter}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="teamLinkLogo"
+                            >
+                              <img
+                                src={twitterLogo}
+                                alt="twitter"
+                                className="teamIcon"
+                              />
+                            </a>
+                          )}
+                          {teamMember.linkedin === "" ? null : (
+                            <a
+                              href={teamMember.linkedin}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="teamLinkLogo"
+                            >
+                              <img
+                                src={linkedinLogo}
+                                alt="linkedin"
+                                className="teamIcon"
+                              />
+                            </a>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-                <div className="profileTextDiv">
-                  <p className="profileSubtitle">Elliot Paschal</p>
-                  <i className="profileSubSubtitle">
-                    UChicago 24
-                    <br />
-                    Juicebox DAO, UMich Research
-                  </i>
-                  <br />
-                  <a
-                    href="https://www.linkedin.com/in/elliot-paschal/"
-                    className="linkedinLink hover"
-                  >
-                    <img
-                      src={linkedinLogo}
-                      className="linkedinLogo"
-                      alt="linkedinLogo"
-                    />
-                  </a>
-                </div>
+                <br />
+                <br />
+                <br />
               </div>
-              <div className="profileDiv">
-                <div className="profileImgDiv">
-                  <img src={lex} className="profileImg" alt="lex" />
-                </div>
-                <div className="profileTextDiv">
-                  <p className="profileSubtitle">Lexington Brill</p>
-                  <i className="profileSubSubtitle">
-                    UChicago 23
-                    <br />
-                    Amazon
-                  </i>
-                  <br />
-                  <a
-                    href="https://www.linkedin.com/in/lexingtonbrill/"
-                    className="linkedinLink hover"
-                  >
-                    <img
-                      src={linkedinLogo}
-                      className="linkedinLogo"
-                      alt="linkedinLogo"
-                    />
-                  </a>
-                </div>
-              </div>
-              <div className="profileDiv">
-                <div className="profileImgDiv">
-                  <img src={simon} className="profileImg" alt="simon" />
-                </div>
-                <div className="profileTextDiv">
-                  <p className="profileSubtitle">Simon Mahns</p>
-                  <i className="profileSubSubtitle">
-                    UChicago 23
-                    <br />
-                    Meta, Globus Labs, CSIL
-                  </i>
-                  <br />
-                  <a
-                    href="https://www.linkedin.com/in/simonmahns/"
-                    className="linkedinLink hover"
-                  >
-                    <img
-                      src={linkedinLogo}
-                      className="linkedinLogo"
-                      alt="linkedinLogo"
-                    />
-                  </a>
-                </div>
-              </div>
-              <div className="profileDiv">
-                <div className="profileImgDiv">
-                  <img src={tim} className="profileImg" alt="tim" />
-                </div>
-                <div className="profileTextDiv">
-                  <p className="profileSubtitle">Timothy Clifford</p>
-                  <i className="profileSubSubtitle">
-                    UChicago 24
-                    <br />
-                    Blockchain Coinvestors & Fifth Era
-                  </i>
-                  <br />
-                  <a
-                    href="https://www.linkedin.com/in/timothy-clifford-125141211/"
-                    className="linkedinLink hover"
-                  >
-                    <img
-                      src={linkedinLogo}
-                      className="linkedinLogo"
-                      alt="link"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="profileDiv">
-                <div className="profileImgDiv">
-                  <img src={james} className="profileImg" alt="james" />
-                </div>
-                <div className="profileTextDiv">
-                  <p className="profileSubtitle">James Slattery</p>
-                  <i className="profileSubSubtitle">
-                    UChicago 23
-                    <br />
-                    USMC, SMB Capital
-                  </i>
-                </div>
-              </div>
-              <div className="profileDiv">
-                <div className="profileImgDiv">
-                  <img src={luke} className="profileImg" alt="james" />
-                </div>
-                <div className="profileTextDiv">
-                  <p className="profileSubtitle">Luke Rosa</p>
-                  <i className="profileSubSubtitle">
-                    UChicago 24
-                    <br />
-                    Passport Labs
-                  </i>
-                </div>
-              </div>
-
-              <div className="profileDiv">
-                <div className="profileImgDiv">
-                  <img src={gwen} className="profileImg" alt="james" />
-                </div>
-                <div className="profileTextDiv">
-                  <p className="profileSubtitle">Gwyneth Howell</p>
-                  <i className="profileSubSubtitle">
-                    UChicago 24
-                    <br />
-                    CPGD
-                  </i>
-                </div>
-              </div>
-              <div className="profileDiv">
-                <div className="profileImgDiv">
-                  <img src={nikko} className="profileImg" alt="james" />
-                </div>
-                <div className="profileTextDiv">
-                  <p className="profileSubtitle">Nikko Wheeler</p>
-                  <i className="profileSubSubtitle">
-                    UChicago 25
-                    <br />
-                    USN
-                  </i>
-                </div>
-              </div>
-            </div>
+            ))}
           </FadeIn>
         </div>
       </div>
     );
   }
 }
-
 export default Team;
